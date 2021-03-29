7bedb406de1099df7bfcf8ec1c8970583ad2f1bc
João Victor Rodrigues Ramalho <joao.ramalho@ee.ufcg.edu.br>
2021-03-29 15:28:59 -0300

    Include lib RTC and comments to use it

--- Arduino/AquarismSuportSystem/AquarismSuportSystem.ino
+++ Arduino/AquarismSuportSystem/AquarismSuportSystem.ino
@@ -1,5 +1,6 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
+#include "RTClib.h" //Biblioteca real time clock
 
 //OneWire onewire(3);
 //DallasTemperature sensor(&onewire);
@@ -10,12 +11,36 @@ int SENSOR_T = 3;
 float ReadCTemperature;
 float PastCTemperature;
 
+RTC_DS1307 rtc; //Objeto do tipo rtc_ds1307
+char daysOfTheWeek[7][12] = {"Domingo", "Segunda", "Terça", "Quarta", "Quinta", "Sexta", "Sábado"};
+
+
 void setup() {
   Serial.begin(9600);
 //  sensor.begin();
 //  sensor.getAddress(addr, 0);
-}
 
+/* Inicialização do sensor, tratamento de erro caso sensor não seja inicializado
+if (! rtc.begin()) { 
+    Serial.println("DS1307 não encontrado"); 
+    while(1); 
+  }
+  if (! rtc.isrunning()) { 
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__))); // Captura data e hora que o código foi compilado 
+
+  }
+  /*
+   
+   */
+     delay(100);
+}
+  /****************************
+Para usar valores de dia, mes e ano é necessário utilizar 
+now.day(),now.month(),now.year(). Já para usar valores de hora minuto e segundo 
+se utiliza, now.hour(), now.minute() e now.second(). Já para um dia da semana especifico 
+se utiliza daysOfTheWeek[now.dayOfTheWeek()]. Informações relevantes para utilizar nos processos de automação
+de horário de ligar e apagar luzes do aquário, tanto como para automatizar o horário de alimentação dos peixes
+  ****************************/
 void loop() {
   
   /*****************************/
